af095b25df3f044c66b7925470eab8d0a16f95e3
Jaehoonoo <justjhoon@gmail.com>
2024-07-26 17:14:53 -0400

    fixed email js to be more secure.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,8 +33,8 @@ const Contact = () => {
 
     emailjs
       .send(
-        "service_bco4qtc",
-        "template_kr05kxa",
+        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
           to_name: "Jhoon Yi",
@@ -42,7 +42,7 @@ const Contact = () => {
           to_email: "jhoonericyi@gmail.com",
           message: form.message,
         },
-        "FatlxdNWfX_x-juT-"
+        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
       )
       .then(
         () => {
